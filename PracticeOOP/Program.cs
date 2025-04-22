81b3864a6fd9e36e6e953443cf0c1561efe5c297
cuthanhcam <cuthanhcam04@gmail.com>
2025-04-22 20:09:56 +0700

    chore: add PracticeOOP

---
+++ PracticeOOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeOOP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x; 
+            Animal dog = new Dog("Dog", "male");
+            dog.Speak();
+        }
+    }
+}
