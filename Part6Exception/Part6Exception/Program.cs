2df48539fd4fa7ed9cfaa83abfb4542b5e39af54
cuthanhcam <cuthanhcam04@gmail.com>
2025-06-14 00:20:25 +0700

    feat: update Part6Exception

--- Part6Exception/Part6Exception/Program.cs
+++ Part6Exception/Part6Exception/Program.cs
@@ -6,29 +6,36 @@
         {
             try
             {
-                Console.Write("Enter a number: ");
+                try
+                {
+                    Console.Write("Enter a number: ");
 
-                int n = int.Parse(Console.ReadLine());
-                int x = 10 / n;
+                    int n = int.Parse(Console.ReadLine());
+                    int x = 10 / n;
 
-                Console.WriteLine(x);
-                throw new Exception("This is a custom exception message.");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Loi chia cho 0");
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("Loi dinh dang");
+                    Console.WriteLine(x);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Loi chia cho 0");
+                    throw new Exception("This is a custom exception message.");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Loi dinh dang, ban phai nhap vao mot so nguyen");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    Console.WriteLine("Ket thuc chuong trinh");
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Loi khac: " + ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Ket thuc chuong trinh");
+                Console.WriteLine(ex.ToString());
             }
         }
     }
