d3621cc261ed05a3b481a1e9e33732255f58afd9
cuthanhcam <cuthanhcam04@gmail.com>
2025-06-16 22:46:56 +0700

    feat: add GamePlatform, MegaStore

---
+++ TestDome/GamePlatform/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GamePlatform
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateFinalSpeed(60, new int[] { 0, 30, 0, -45, 0 }));
+        }
+
+        public static double CalculateFinalSpeed(double initialSpeed, int[] inclinations)
+        {
+            double speed = initialSpeed;
+            for (int i = 0; i < inclinations.Length; i++)
+            {
+                speed -= inclinations[i];
+
+                if (speed < 0)
+                {
+                    speed = 0;
+                }
+            }
+            return speed;
+        }
+    }
+}
---
+++ TestDome/MegaStore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class MegaStore
+{
+    public enum DiscountType
+    {
+        Standard, // 6
+        Seasonal, // 12
+        Weight // <= 10 = 6, > 10 = 18
+    }
+
+    public static double GetDiscountedPrice(double cartWeight,
+                                            double totalPrice,
+                                            DiscountType discountType)
+    {
+        double discountedPrice = totalPrice;
+        if (discountType == DiscountType.Standard)
+        {
+            discountedPrice *= 0.94;
+        }
+        else if (discountType == DiscountType.Seasonal)
+        {
+            discountedPrice *= 0.88;
+        }
+        else if (discountType == DiscountType.Weight)
+        {
+            if (cartWeight <= 10)
+            {
+                discountedPrice *= 0.94;
+            }
+            else
+            {
+                discountedPrice *= 0.82;
+            }
+        }
+        return discountedPrice;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetDiscountedPrice(12, 100, DiscountType.Weight));
+    }
+}
