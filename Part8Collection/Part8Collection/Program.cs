13edff8d9006c95810a272ed5de4a3ecdc246a2f
cuthanhcam <cuthanhcam04@gmail.com>
2025-06-25 21:43:00 +0700

    feat: add Part8Collection

---
+++ Part8Collection/Part8Collection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part8Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
