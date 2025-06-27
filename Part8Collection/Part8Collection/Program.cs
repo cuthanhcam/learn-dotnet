b51080e90a913b278496af48f7de28caa329cf9b
cuthanhcam <cuthanhcam04@gmail.com>
2025-06-27 13:05:28 +0700

    feat: modified Part8Collection, test by txt

--- Part8Collection/Part8Collection/Program.cs
+++ Part8Collection/Part8Collection/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Part8Collection
+﻿
+namespace Part8Collection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var list = new List<string>();
+
+            ReadString(list);
+            Print(list);
+            Sort(list);
+            Print(list);
+        }
+
+        private static void Sort(List<string> list)
+        {
+            for (int i = 0 ; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i].CompareTo(list[j]) > 0)
+                    {
+                        var temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+
+                }
+            }
+        }
+
+        private static void Print(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void ReadString(List<string> list)
+        {
+            string? s;
+            do
+            {
+                s = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(s))
+                {
+                    list.Add(s);
+                }
+            } while (!string.IsNullOrWhiteSpace(s));
         }
     }
 }
