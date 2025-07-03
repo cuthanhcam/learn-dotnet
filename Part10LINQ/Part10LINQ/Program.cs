1194f2de2777e511380fb85380966119fedf91c6
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-03 15:26:12 +0700

    feat: add Part10LINQ, modified TestDome

---
+++ Part10LINQ/Part10LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Part10LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 3, 1, 4, 1, 5, 9 };
+
+            IEnumerable<int> scoreQuery = from score in scores
+                                          where score % 2 == 1
+                                          orderby score ascending
+                                          select score;
+            foreach (var item in scoreQuery)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
