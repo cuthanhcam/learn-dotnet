750a5afe9b89d32a2956914ed518709689c2c167
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-09 12:14:07 +0700

    feat: modified PART10LINQ

--- Part10LINQ/Part10LINQ/Program.cs
+++ Part10LINQ/Part10LINQ/Program.cs
@@ -8,11 +8,23 @@
 
             Print(dataSource);
 
-            var filteredData = from n in dataSource
-                               where n <= 0
-                               select n;
+            // Query syntax example
+            //var filteredData = from n in dataSource
+            //                   where GreaterThanZero(n) && n % 2 == 0
+            //                   select n;
+
+            // Method syntax example
+            var filteredData = dataSource.Where(n => GreaterThanZero(n) && n % 2 == 0);
 
             Print(filteredData);
+
+            Console.WriteLine($"Count: {filteredData.Count()}, Sum: {filteredData.Sum()}");
+        }
+
+        static bool GreaterThanZero(int n)
+        {
+            //Console.WriteLine($"{n} > 0 = {n > 0}");
+            return n > 0;
         }
 
         static IEnumerable<int> GetIntNumbers()
@@ -25,7 +37,7 @@
         {
             foreach (var value in numbers)
             {
-                Console.Write($"{value} ");
+                Console.WriteLine($"{value} ");
             }
             Console.WriteLine();
         }
---
+++ Part10LINQ/linq2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace linq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = GetStudents();
+            Print(students.Where(s => s.YoB == 2000));
+            Console.WriteLine("----");
+            Print(students.OrderByDescending(s => s.YoB).OrderBy(s => s.Name));
+
+            foreach (var student in students.OrderBy(s => s.Name).Select(s => s.YoB))
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        static void Print(IEnumerable<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Print(student);
+            }
+        }
+
+        private static void Print(Student student)
+        {
+            Console.WriteLine($"Name: {student.Name}, City: {student.City}, Year of Birth: {student.YoB}");
+        }
+
+        static IEnumerable<Student> GetStudents()
+        {
+            return new Student[]
+            {
+                new Student { Name = "Alice", City = "New York", YoB = 2000 },
+                new Student { Name = "Bob", City = "Los Angeles", YoB = 1999 },
+                new Student { Name = "Charlie", City = "Chicago", YoB = 2001 },
+                new Student { Name = "David", City = "Houston", YoB = 2000 }
+            };
+
+        }
+    }
+}
