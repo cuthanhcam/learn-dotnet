1b1b1c13b227faed3ed68183e34fb985926673c0
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-10 19:45:29 +0700

    feat: update example for use class in same assembly

---
+++ PART11OOP/ClassLibrary/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PART11OOP
+{
+    public class Printer
+    {
+        internal void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- PART11OOP/PART11OOP/Program.cs
+++ PART11OOP/PART11OOP/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var printer = new Printer();
+            printer.Print("Hello, World!");
         }
     }
 }
