7e2da3aaf90308e07281d8221a528329e78f7b3d
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-10 20:24:25 +0700

    feat: initial example for constructor

--- PART11OOP/ClassLibrary/Printer.cs
+++ PART11OOP/ClassLibrary/Printer.cs
@@ -4,10 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PART11OOP
+namespace ClassLibrary
 {
     public class Printer
     {
+        private string _message;
+
+        public required int Page { get; set; }
+
+        public Printer()
+        {
+            _message = string.Empty;
+            Console.WriteLine("Printer instance created.");
+        }
+
+        public Printer(string message)
+        {
+            _message = message;
+            Console.WriteLine($"Printer instance created with message: {message}");
+        }
+
         public void Print(string message)
         {
             Console.WriteLine(message);
--- PART11OOP/PART11OOP/Program.cs
+++ PART11OOP/PART11OOP/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace PART11OOP
+﻿using ClassLibrary;
+using ClassLibrary.P;
+
+namespace PART11OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var printer = new Printer();
+            var printer = new Printer("Cam")
+            {
+                Page = 100
+            };
+
+            var laserPrinter = new LaserPrinter()
+            {
+                Page = 200
+            };
+
             printer.Print("Hello, World!");
         }
     }
