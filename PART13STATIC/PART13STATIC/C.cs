5c39ff7e62d9e872d47b8e4654322e3b4765c2fd
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-12 14:50:37 +0700

    feat: example for value

---
+++ PART13STATIC/PART13STATIC/C.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PART13STATIC
+{
+    internal class C
+    {
+        public int x;
+
+    }
+}
---
+++ PART13STATIC/PART13STATIC/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PART13STATIC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new C() { x = 111 }; 
+
+            var c2 = new C() { x = 222 };
+
+            Console.WriteLine(c1.x);
+
+            Console.WriteLine(c2.x);
+
+            c2.x = 333;
+
+            Console.WriteLine(c1.x);
+
+            Console.WriteLine(c2.x);
+        }
+    }
+}
