12459f5fd339f1ae6ba57f34b283d83687fa4faa
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-12 19:23:52 +0700

    feat: initial PART14Interface

---
+++ PART14Interface/PART14Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PART14Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
