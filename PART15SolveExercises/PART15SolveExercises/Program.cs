e38271de26191b47713a536a0f9d833c8c1d916d
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-13 14:03:31 +0700

    feat: initial PART15SolveExercises

---
+++ PART15SolveExercises/PART15SolveExercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PART15SolveExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
