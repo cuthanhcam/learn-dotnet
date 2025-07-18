7ba60a995abd0f81de6d72ff48a6d3775570e0aa
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-18 23:25:34 +0700

    feat: add NCommands

---
+++ NCommands/NFind/FindOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFind
+{
+    internal class FindOptions
+    {
+        public string StringToFind { get; set; } = string.Empty;
+        public bool IsCaseSensitive { get; set; } = false;
+        public bool FindDontConstain { get; set; } = false;
+        public bool CountMode { get; set; } = false;
+        public bool ShowLineNumbers { get; set; } = false;
+        public bool SkipOfflineFiles { get; set; } = true;
+        public string Path { get; set; } = string.Empty;
+        public bool HelpMode { get; set; } = false;
+    }
+}
---
+++ NCommands/NFind/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace NFind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("FIND: Parameter format not correct");
+                return;
+            }
+
+            var fileOptions = BuildOptions(args);
+        }
+
+        private static object BuildOptions(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
