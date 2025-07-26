59bd0a0fa601304916ff15811a8bd5cb20fc7633
cuthanhcam <cuthanhcam04@gmail.com>
2025-07-26 21:28:09 +0700

    feat: completed FirstADOApp

--- PART17ADONET/PART17ADONET/Program.cs
+++ PART17ADONET/PART17ADONET/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace PART17ADONET
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace PART17ADONET
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TestDBConnection();
+        }
+
+        public static void TestDBConnection()
+        {
+            using var conn = new SqlConnection("Data Source=JERK\\CAMDB;Initial Catalog=HR;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();    
+            
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT employee_id, email FROM employees";
+
+            using var reader = cmd.ExecuteReader();
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Employee ID: {reader.GetInt32(0)}, Email: {reader.GetString(1)}");
+                }
+            }
+           
+            conn.Close();
         }
     }
 }
---
+++ PART17ADONET/PART17ADONETTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PART17ADONET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PART17ADONET.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void TestDBConnectionTest()
+        {
+            Program.TestDBConnection();
+        }
+    }
+}
