9fc33b5fe50dc9ee9034c33e622e48a103b86420
Nu <cuthanhcam04@gmail.com>
2026-01-13 15:54:44 +0700

    chore: update legacy-code

---
+++ legacy-code/multithreads-test/multithreads-test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace optimize_loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal n = 10_000_000_000_000;
+            int numThreads = 12;
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Sum sum = new Sum();
+            decimal total = sum.CalculateSum(n, numThreads);
+            sw.Stop();
+
+            Console.WriteLine($"Total: {total}");
+            Console.WriteLine($"Time: {sw.Elapsed.TotalSeconds} seconds");
+        }
+    }
+}
---
+++ legacy-code/multithreads-test/multithreads-test/Sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace optimize_loop
+{
+    public class Sum
+    {
+        public decimal CalculateSum(decimal n, int numThreads)
+        {
+            decimal[] partialSums = new decimal[numThreads];
+            Parallel.For(0, numThreads, p =>
+            {
+                decimal start = p * (n / numThreads);
+                decimal end = (p == numThreads - 1) ? n : start + (n / numThreads);
+                decimal subtotal = 0;
+                for (decimal i = start; i < end; i++)
+                {
+                    subtotal += i;
+                }
+                partialSums[p] = subtotal;
+            });
+            return partialSums.Sum();
+        }
+    }
+}
