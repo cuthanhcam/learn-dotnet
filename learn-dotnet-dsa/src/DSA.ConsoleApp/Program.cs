7f824f25e23d07bf0d2fff6b38d51e787303d888
Nu <cuthanhcam04@gmail.com>
2026-01-21 23:51:41 +0700

    update

--- learn-dotnet-dsa/src/DSA.ConsoleApp/Program.cs
+++ learn-dotnet-dsa/src/DSA.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DSA.Core.Algorithms.Searching;
+using DSA.Core.DataStructures.Arrays;
 
 namespace DSA.ConsoleApp
 {
@@ -6,34 +7,12 @@ namespace DSA.ConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Binary Search Demo");
+            Console.WriteLine("Data Structures Array Remove Duplicates Demo");
 
-            int[] nums = { 1, 3, 5, 7, 9, 11, 13 };
-            int target = 11;
-
-            for (int i = 0; i <= nums.Length - 1; i++)
-            {
-                Console.Write($"{nums[i]} ");
-            }
-
-            Console.WriteLine($"\nTarget: {target}");
-            
-            // Iterative
-            int index = BinarySearch.BinarySearchIterative(nums, target);
-
-            if (index != -1)
-            {
-                Console.WriteLine($"Iterative search resul: index = {index}");
-            }
-            else
-            {
-                Console.WriteLine($"{target} not found");
-            }
-
-            // Recursive
-            int recursiveIndex = BinarySearch.BinarySearchRecursive(nums, target);
-
-            Console.WriteLine($"Recursive search result: index = {recursiveIndex}");
+            int[] nums = { 1, 1, 2, 2, 3, 4 };
+ 
+            Console.WriteLine($"Two pointer result: {TwoPointers.RemoveDuplicates(nums)}");
+            Console.WriteLine($"Two pointer result: {TwoPointers.RemoveDuplicatesHashSet(nums)}");
         }
     }
 }
