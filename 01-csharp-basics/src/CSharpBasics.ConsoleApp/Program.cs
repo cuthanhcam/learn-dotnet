922080f6d609746a9cd1bf90f71dbb48a73c71cd
Nu <cuthanhcam04@gmail.com>
2026-03-29 22:42:18 +0700

    feat(01-csharp-basics): update console app and variables lesson content

--- 01-csharp-basics/src/CSharpBasics.ConsoleApp/Program.cs
+++ 01-csharp-basics/src/CSharpBasics.ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpBasics.Examples.Variables;
+
+namespace CSharpBasics.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("C# Fundamentasl Demo Runner");
+            Console.WriteLine(new string('-', 50));
+
+            RunSection("Variables & Types", static () =>
+            {
+                VariablesExamples.Run();
+
+            });
+        }
+        
+        public static void RunSection(string title, Action action)
+        {
+            Console.WriteLine($"\n{new string('-', 20)} {title} {new string('-', 20)}");
+            action();
+        }
+    }
+}
+
